54ec65424aeb24e1b0bb47b51742f2c3e8363c56
Nitsan.Maman <nit.maman@gmail.com>
2024-05-28 15:03:01 +0300

    Deploy Backend

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const Form = require('./models/Form');
 const cors = require('cors');
 const app = express();
 app.use(express.json({limit : 52428800}))
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://Evidentify.onrender.com"]
+}));
 
 const bodyParser = require('body-parser');
 
